6dba5eb5b984248b12d28f3d686e0b45d2df3398
Nikolay Brovkin <bronik004@gmail.com>
2022-12-12 10:50:04 +0300

    feat: added active category

--- src/components/categories/categories.jsx
+++ src/components/categories/categories.jsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Categories = () => {
+  const [active, setActive] = useState('');
+
+  const handleActive = value => {
+    setActive(value);
+  };
   return (
-    <div className="categories">
+    <div className='categories'>
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        <li
+          onClick={() => handleActive('Все')}
+          className={active === 'Все' ? 'active' : ''}
+        >
+          Все
+        </li>
+        <li
+          onClick={() => handleActive('Мясные')}
+          className={active === 'Мясные' ? 'active' : ''}
+        >
+          Мясные
+        </li>
+        <li
+          onClick={() => handleActive('Вегетарианская')}
+          className={active === 'Вегетарианская' ? 'active' : ''}
+        >
+          Вегетарианская
+        </li>
+        <li
+          onClick={() => handleActive('Гриль')}
+          className={active === 'Гриль' ? 'active' : ''}
+        >
+          Гриль
+        </li>
+        <li
+          onClick={() => handleActive('Острые')}
+          className={active === 'Острые' ? 'active' : ''}
+        >
+          Острые
+        </li>
+        <li
+          onClick={() => handleActive('Закрытые')}
+          className={active === 'Закрытые' ? 'active' : ''}
+        >
+          Закрытые
+        </li>
       </ul>
     </div>
   );
