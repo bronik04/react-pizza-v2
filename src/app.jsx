e558c039b400ecf8b8b5b3fc3fbd505cf8762c0d
Nikolay Brovkin <bronik004@gmail.com>
2022-12-12 16:32:33 +0300

    feat: added skeleton

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './scss/app.scss';
+import axios from 'axios';
 import Header from './components/header/header';
 import Categories from './components/categories/categories';
 import Sort from './components/sort/sort';
 import PizzaBlock from './components/pizza-block/pizza-block';
-import { pizzas } from './utils/consts';
+import { Skeleton } from './components/pizza-block/skeleton';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const gerItems = async () => {
+      const res = await axios.get(
+        'https://6396f81d77359127a0282b32.mockapi.io/items',
+      );
+      setItems(res.data);
+      setIsLoading(false);
+    };
+    gerItems();
+  }, []);
+
   return (
     <div className='App'>
       <div className='wrapper'>
@@ -19,9 +34,16 @@ function App() {
             </div>
             <h2 className='content__title'>Все пиццы</h2>
             <div className='content__items'>
-              {pizzas.map(pizza => (
-                <PizzaBlock key={pizza.title} {...pizza} />
-              ))}
+              {isLoading
+                ? [...new Array(6)].map((_, index) => (
+                    <Skeleton key={index} />
+                  ))
+                : items.map(pizza => (
+                    <PizzaBlock
+                      key={pizza.title}
+                      {...pizza}
+                    />
+                  ))}
             </div>
           </div>
         </div>
