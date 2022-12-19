a77acdc96f9d8971247788865f2c5095e5b9fb35
Nikolay Brovkin <bronik004@gmail.com>
2022-12-19 10:02:43 +0300

    feat: update readme

--- src/utils/getCartFromLocalStorage.ts
+++ src/utils/getCartFromLocalStorage.ts
@@ -1,11 +1,12 @@
 import { calcTotalPrice } from './calcTotalPrice';
+import {CartItemProps} from "./consts";
 
 export const getCartFromLocalStorage = () => {
   const data = localStorage.getItem('cart');
   const items = data ? JSON.parse(data) : [];
   const totalPrice = calcTotalPrice(items);
     return {
-      items,
+      items: items as CartItemProps[],
       totalPrice,
     };
 };
